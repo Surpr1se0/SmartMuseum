ce70f9c2c039da193aaed1747ebcf8f71ef92cb7
Surpr1se0 <franciscoggouveia@gmail.com>
2023-11-26 17:20:38 +0000

    add tabs to management

--- Management/app-management.js
+++ Management/app-management.js
@@ -9,6 +9,27 @@ var humidty;
 var alarm;
 var smoke;
 
+function changeTab(event, arduinoNum) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(arduinoNum).style.display = "block";
+  event.currentTarget.className += " active";
+} 
+
 const client = mqtt.connect("mqtt://localhost:9001", options);
 
 function OnConnect() {
